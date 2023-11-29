ed7fee63466df84541ef5d4b76f6174161c4f257
PeytonCurr <peyton.curr@gmail.com>
2023-11-29 15:56:18 -0700

    Created new Database and added account table

--- dotNetGregsListVue/dbSetup.sql
+++ dotNetGregsListVue/dbSetup.sql
@@ -1,8 +1,11 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+-- Active: 1686681110274@@rds-mysql-10mintutorial.c81ovpu4pgjf.us-west-2.rds.amazonaws.com@3306@dotNetGregsListVue
+
+CREATE TABLE
+    IF NOT EXISTS accounts(
+        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name varchar(255) COMMENT 'User Name',
+        email varchar(255) COMMENT 'User Email',
+        picture varchar(255) COMMENT 'User Picture'
+    ) default charset utf8mb4 COMMENT '';
