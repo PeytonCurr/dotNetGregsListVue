3e82b6f2d9257f6dde3567ceb051f1e0808dd564
PeytonCurr <peyton.curr@gmail.com>
2023-11-29 17:02:44 -0700

    Created Backend Car Model Also Added Profile to Account

---
+++ dotNetGregsListVue/Models/Car.cs
@@ -0,0 +1,14 @@
+namespace dotNetGregsListVue.Models;
+
+public class Car
+{
+  public int Id { get; set; }
+  public string Make { get; set; }
+  public string Model { get; set; }
+  public string ImgUrl { get; set; }
+  public string Body { get; set; }
+  public int? Price { get; set; }
+  public string Description { get; set; }
+  public string CreatorId { get; set; }
+  public Profile Seller { get; set; }
+}
